3acc6363a0d7eec533a4b97fc115f6304aa2ff53
dengwy <dengwyxa@gmail.com>
2020-06-29 17:43:05 +0900

    ViewPager Demo(first Kotlin)

---
+++ app/src/main/java/com/example/viewpagerdemo/ScaleFragment.kt
@@ -0,0 +1,35 @@
+package com.example.viewpagerdemo
+
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_scale.*
+import kotlin.random.Random
+
+/**
+ * A simple [Fragment] subclass.
+ * create an instance of this fragment.
+ */
+class ScaleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scale, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        imageViewScale.setOnClickListener {
+            val factor: Float = if (Random.nextBoolean()) 0.1f else -0.1f
+            ObjectAnimator.ofFloat(it, "scaleX", it.scaleX + factor).start()
+            ObjectAnimator.ofFloat(it, "scaleY", it.scaleY + factor).start()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/viewpagerdemo/TranslateFragment.kt
@@ -0,0 +1,32 @@
+package com.example.viewpagerdemo
+
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_translate.*
+import kotlin.random.Random
+
+/**
+ * A simple [Fragment] subclass.
+ * create an instance of this fragment.
+ */
+class TranslateFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_translate, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        imageViewTranslate.setOnClickListener {
+            ObjectAnimator.ofFloat(it, "translationX", it.translationX + Random.nextInt(-100, 100)).start()
+        }
+    }
+}
